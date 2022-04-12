bd98b37f79d694168685ba1bcf38a36ac8cfe1b4
MohamedAymanNeo <az.mohamedayman12@gmail.com>
2022-04-12 04:10:27 +0200

    push updates till cards

---
+++ tailwind-default.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './index.html',
+  ],
+  extend: {
+    colors: {
+      primary: '#FF6363',
+      secondary: {
+        100: '#E2E2D5',
+        200: '#888883',
+      }
+    },
+    fontFamily: {
+      body: ['Nunito']
+    }
+  },
+  plugins: [],
+}
